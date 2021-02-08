dd94a7c30babc96d5c742f970b3f1178cc694783
icingfire <48239025+icingfire@users.noreply.github.com>
2021-02-08 15:32:27 +0800

    Create domain2ip.sh

---
+++ domain2ip.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# $1  file contains domain names
+
+if [ ! -f "$1" ]; then
+        echo "file $1 not found, exit"
+        exit
+fi
+
+while read one_host
+do
+        ret_ip=$(ping -n -c 1 ${one_host} |head -1|awk '{print $3}'|sed 's/[\(\)]//g')
+        [ -n "${ret_ip}" ] && echo "${ret_ip}  ${one_host}"
+done < $1
