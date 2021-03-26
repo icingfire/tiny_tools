ab546927e3e28b66309cc213634bce77dd9a75cf
icingfire <48239025+icingfire@users.noreply.github.com>
2021-03-26 15:29:49 +0800

    Update domain2ip.sh
    并行ping，每秒起10个子进程

--- domain2ip.sh
+++ domain2ip.sh
@@ -2,12 +2,12 @@
 # $1  file contains domain names
 
 if [ ! -f "$1" ]; then
-        echo "file $1 not found, exit"
-        exit
+    echo "file $1 not found, exit"
+    exit
 fi
 
 while read one_host
 do
-        ret_ip=$(ping -n -c 1 ${one_host} |head -1|awk '{print $3}'|sed 's/[\(\)]//g')
-        [ -n "${ret_ip}" ] && echo "${ret_ip}  ${one_host}"
+    (ret_ip=$(ping -n -c 1 ${one_host} |head -1|awk '{print $3}'|sed 's/[\(\)]//g'); [ -n "${ret_ip}" ] && echo "${ret_ip}  ${one_host}" >> "${1}".ip ) &
+    sleep 0.1
 done < $1
