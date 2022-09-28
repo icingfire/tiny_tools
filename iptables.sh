a6f8b92daa0e1047777b7947b04a5cd87d92b94b
icingfire <48239025+icingfire@users.noreply.github.com>
2022-09-28 14:42:53 +0800

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -8,9 +8,10 @@ iptables -F
 SSH_PORT=22
 white_list=(6.6.6.6 8.8.8.8)
 
+iptables -I INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --dport "${SSH_PORT}"  -j ACCEPT
 for one in "${!white_list[@]}";
 do
   iptables -A INPUT -p tcp -s "${white_list[${one}]}" -j ACCEPT
 done
-iptables -A INPUT -j DROP
+iptables -A INPUT -p tcp -j DROP
